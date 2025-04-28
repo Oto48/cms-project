6f439dfe627ad06b87bd83f51985b3d7a949af76
oto48 <otchokhonelidze4@gmail.com>
2025-04-29 00:54:44 +0400

    updated helpers file with new directory functions

--- helpers.php
+++ helpers.php
@@ -1,11 +1,32 @@
 <?php 
 
-function base_url() {
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
-    $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
-    $base_url = $protocol . $_SERVER['HTTP_HOST'] . $basePath;
-    
-    return $base_url;
+function base_url($path = ""){
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== "off" ? 'https://' : 'http://';
+    $host = $_SERVER['HTTP_HOST'];
+    $baseUrl = $protocol . $host . '/' . PROJECT_DIR;
+    return $baseUrl . '/' . ltrim($path, '/');
+}
+
+function base_path($path = ""){
+    $rootPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . PROJECT_DIR;
+    return $rootPath . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
+}
+
+function uploads_path($filename = ''){
+    return base_path('uploads' . DIRECTORY_SEPARATOR . $filename);
+}
+
+function uploads_url($filename = ''){
+    return base_path('uploads/' . ltrim($filename, '/'));
+}
+
+function asset_url($path = ""){
+    return base_url('assets/') . ltrim($path, '/');
+}
+
+function redirect($url){
+    header('Location: ' . base_url($url));
+    exit;
 }
 
 ?>
